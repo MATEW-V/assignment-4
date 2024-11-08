ef1cc6d45c9b41b9f6aa7be69d643d6ac8f178b6
Matthew Vuong <matthewevuong@gmail.com>
2024-11-08 11:58:44 -0500

    srtarted amb case

--- src/components/AmbiguousCase.jsx
+++ src/components/AmbiguousCase.jsx
@@ -1,2 +1,31 @@
 import { useState } from 'react';
 import './AmbiguousCase.css';
+
+function AmbiguousCase() {
+    const [angleA, setA] = useState(0);
+    const [sideA, setB] = useState(0);
+    const [sideB, setC] = useState(0);
+    const [tritype, setTri] = useState("Press Calulate");
+
+
+
+function ambigCase(e){
+    e.preventDefault();
+
+}
+
+return(
+    <form onSubmit={(e) => ambigcase(e)}>
+    <h1 class="header">Heron's Formula</h1>
+    <label for="sa">Side A: </label>
+    <input type="number" id="sa" name="SideA"></input>
+    <label for="sb">Side B: </label>
+    <input type="number" id="sb" name="SideB"></input>
+    <label for="sc">Side C: </label>
+    <input type="number" id="sc" name="SideC"></input>
+    <label for="ahf">Area:</label>
+    <input type="number" id="ahf" name="areahf" readonly></input>
+    <button type="submit" id="chf" class="calculate">Calculate</button>
+    </form>
+)
+}
